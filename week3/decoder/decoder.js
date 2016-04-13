3285f1751ecc43ef3b66dc7047febab1bad489dd
Samuel Frade <SamuelSFrade@gmail.com>
2016-04-13 11:48:41 -0400

    Finish Javascript decoder exercise.

---
+++ week3/decoder/decoder.js
@@ -0,0 +1,20 @@
+function decoder(words){
+    secretMessage = ""
+        for (var i = 0; i < words.length; i++){
+            secretMessage += (words[i][i % 5]);
+        }
+        return secretMessage;
+}
+
+var words = [
+    "dead",
+    "bygone",
+    "landing",
+    "cheaply",
+    "assumed",
+    "incorrectly",
+    "attention",
+    "agent"
+];
+
+console.log(decoder(words));
