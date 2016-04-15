84a877bb4a74b8b63faae292b265cf9cd0792dda
Samuel Frade <SamuelSFrade@gmail.com>
2016-04-15 12:24:56 -0400

    Added users to quiz game

--- week3/quiz_game/quiz.js
+++ week3/quiz_game/quiz.js
@@ -6,43 +6,69 @@ var rounds;
 
 var question1 = new Question(
     "What is your name?\n >",
-    "Lancelot"
+    "Lancelot",
+    1
     );
 
 var question2 = new Question(
     "What is your quest?\n >",
-    "To find the grail."
+    "To find the grail.",
+    1
     );
 
 var question3 = new Question(
     "What is your favorite color?\n >",
-    "Blue"
+    "Blue",
+    1
     );
 
 var question4 = new Question(
     "What is the average airspeed velocity of an unladen swallow?\n >",
-    "African or European swallow?"
+    "African or European swallow?",
+    1
     );
+
+var bob = new User("Bob");
+var rob = new User("Rob");
+var betty = new User("Betty");
+var charlotte = new User("Charlotte");
+
+var users = [bob, rob, betty, charlotte];
 
 var questions = [question1, question2, question3, question4];
 
 var current_question = question1;
 
-function Question(description, answer){
+var current_user;
+
+function Question(description, answer, points){
     this.description = description;
     this.answer = answer;
+    this.points = points;
 }
 
 Question.prototype.checkAnswer = function(answer) {
     return (this.answer === answer);
 };
 
+function User(name){
+    this.name = name;
+    this.score = 0;
+}
+
+User.prototype.addToScore = function(num) {
+    this.score += num;
+};
 
 function setup(err, num){
         if (err) {throw err;}
         rounds = parseInt(num);
         if (rounds <= 0) {console.log("Game Over. Goodbye.")}
-        else {read({prompt: current_question.description}, quiz);}
+        else {
+            current_user = users[rounds % users.length];
+            console.log ("It is currently " + current_user.name + "'s turn.")
+            read({prompt: current_question.description}, quiz);
+        }
     }
 
 
@@ -50,7 +76,9 @@ function quiz(err, answer){
     if (err) {throw err}
 
     if (current_question.checkAnswer(answer)){
+        current_user.addToScore(current_question.points);
         console.log("Good Job.");
+        console.log(current_user.name + " now has " + current_user.score + " points.");
     }
     else {
         console.log("AAAHHH!!!");
@@ -62,9 +90,12 @@ function quiz(err, answer){
         console.log("Game Over.  Goodbye.");
     }
     else {
+        current_user = users[rounds % users.length];
         current_question = questions[Math.floor(Math.random() * questions.length)];
+        console.log("It is now " + current_user.name + "'s turn.");
         read({prompt: current_question.description}, quiz);
     }
 }
+
 
 read(setup_question, setup);
