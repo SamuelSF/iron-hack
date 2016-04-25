df1abe7989ac8661849f2a19233fea2021ab9221
Samuel Frade <SamuelSFrade@gmail.com>
2016-04-25 15:13:48 -0400

    Finish pizza builder iteration one.

--- week5/pizza-builder/src/pizza.js
+++ week5/pizza-builder/src/pizza.js
@@ -1,1 +1,4 @@
 // Write your Pizza Builder JavaScript in this file.
+$(".btn-pepperonni").on("click", function(){$(".pep").toggle();});
+$(".btn-green-peppers").on("click", function(){$(".green-pepper").toggle();});
+$(".btn-mushrooms").on("click", function(){$(".mushroom").toggle();});
