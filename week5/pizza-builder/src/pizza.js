c6fdd4be2a36a361703a84a50da4ff8429643a32
Samuel Frade <SamuelSFrade@gmail.com>
2016-04-25 18:08:37 -0400

    Finish pizza builder.

--- week5/pizza-builder/src/pizza.js
+++ week5/pizza-builder/src/pizza.js
@@ -2,9 +2,69 @@
 $(".pep").hide();
 $(".green-pepper").hide();
 $(".mushroom").hide();
+$(".pep-list").hide();
+$(".mush-list").hide();
+$(".green-list").hide();
+$(".sauce-list").hide();
+$(".gluten-list").hide();
 
-$(".btn-pepperonni").on("click", function(){$(".pep").toggle(); $(".btn-pepperonni").toggleClass("active");});
-$(".btn-green-peppers").on("click", function(){$(".green-pepper").toggle(); $(".btn-green-peppers").toggleClass("active");});
-$(".btn-mushrooms").on("click", function(){$(".mushroom").toggle(); $(".btn-mushrooms").toggleClass("active");});
-$(".btn-sauce").on("click", function(){$(".sauce").toggleClass("sauce-white"); $(".btn-sauce").toggleClass("active");});
-$(".btn-crust").on("click", function(){$(".crust").toggleClass("crust-gluten-free"); $(".btn-crust").toggleClass("active");});
+function pepperonni_cb(){
+    $(".pep").toggle();
+    $(".btn-pepperonni").toggleClass("active");
+    $(".pep-list").toggle();
+    change_total()
+}
+
+function green_pepper_cb(){
+    $(".green-pepper").toggle();
+    $(".btn-green-peppers").toggleClass("active");
+    $(".green-list").toggle();
+    change_total()
+}
+
+function mushroom_cb(){
+    $(".mushroom").toggle();
+    $(".btn-mushrooms").toggleClass("active");
+    $(".mush-list").toggle();
+    change_total()
+}
+
+function sauce_cb(){
+    $(".sauce").toggleClass("sauce-white");
+    $(".btn-sauce").toggleClass("active");
+    $(".sauce-list").toggle();
+    change_total();
+}
+
+function crust_cb(){
+    $(".crust").toggleClass("crust-gluten-free");
+    $(".btn-crust").toggleClass("active");
+    $(".gluten-list").toggle();
+    change_total();
+}
+
+function change_total(){
+    var price = 10;
+    if ($(".btn-crust").hasClass("active")){
+        price += 5;
+    }
+    if ($(".btn-pepperonni").hasClass("active")){
+        price += 1
+    }
+    if ($(".btn-sauce").hasClass("active")){
+        price += 3
+    }
+    if ($(".btn-mushrooms").hasClass("active")){
+        price += 1
+    }
+    if ($(".btn-green-peppers").hasClass("active")){
+        price += 1
+    }
+    $(".total").text("$ " + price);
+}
+
+$(".btn-pepperonni").on("click", pepperonni_cb);
+$(".btn-green-peppers").on("click", green_pepper_cb);
+$(".btn-mushrooms").on("click", mushroom_cb);
+$(".btn-sauce").on("click", sauce_cb );
+$(".btn-crust").on("click", crust_cb);
