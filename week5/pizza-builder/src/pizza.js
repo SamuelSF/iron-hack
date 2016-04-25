e881e26c38d7024716a4e406b93df68bb929631e
Samuel Frade <SamuelSFrade@gmail.com>
2016-04-25 15:51:50 -0400

    Finish fourth iteration of pizza builder.

--- week5/pizza-builder/src/pizza.js
+++ week5/pizza-builder/src/pizza.js
@@ -1,4 +1,10 @@
 // Write your Pizza Builder JavaScript in this file.
-$(".btn-pepperonni").on("click", function(){$(".pep").toggle();});
-$(".btn-green-peppers").on("click", function(){$(".green-pepper").toggle();});
-$(".btn-mushrooms").on("click", function(){$(".mushroom").toggle();});
+$(".pep").hide();
+$(".green-pepper").hide();
+$(".mushroom").hide();
+
+$(".btn-pepperonni").on("click", function(){$(".pep").toggle(); $(".btn-pepperonni").toggleClass("active");});
+$(".btn-green-peppers").on("click", function(){$(".green-pepper").toggle(); $(".btn-green-peppers").toggleClass("active");});
+$(".btn-mushrooms").on("click", function(){$(".mushroom").toggle(); $(".btn-mushrooms").toggleClass("active");});
+$(".btn-sauce").on("click", function(){$(".sauce").toggleClass("sauce-white"); $(".btn-sauce").toggleClass("active");});
+$(".btn-crust").on("click", function(){$(".crust").toggleClass("crust-gluten-free"); $(".btn-crust").toggleClass("active");});
