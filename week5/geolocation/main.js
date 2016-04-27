74db808191b3446edc06149a9b3ea18b43126f46
Samuel Frade <SamuelSFrade@gmail.com>
2016-04-27 12:28:53 -0400

    Add map to geolocation page.

---
+++ week5/geolocation/main.js
@@ -0,0 +1,24 @@
+$(document).on("ready", function(){
+    if("geolocation" in navigator){
+        console.log("geolocation is available");
+
+        navigator.geolocation.getCurrentPosition(displayPosition, showPositionError );
+    }
+    else {
+        alert("geolocation IS NOT available")
+    }
+});
+
+function displayPosition(data){
+    console.log("Got position!");
+    console.log(data);
+    $(".js-set-latitude").text(data.coords.latitude);
+    $(".js-set-longitude").text(data.coords.longitude);
+    $("body").append(`<img src="
+        http://maps.googleapis.com/maps/api/staticmap?size=1000x1000&zoom=20&center=${data.coords.latitude},${data.coords.longitude}">`);
+}
+
+function showPositionError(error){
+    console.log("Failed to get position :( ");
+    console.log(error);
+}
